e023da5703d83709fde51472aa32b7e00843d6f2
Jason Press <therealcannedtuna@gmail.com>
2025-06-12 10:43:01 -0700

    Added VGG loss

--- network.py
+++ network.py
@@ -7,7 +7,7 @@ import torch.nn as nn
 import torch.nn.functional as F  # Needed for GELU in TransformerEncoderLayer
 import torchvision.transforms.functional as TF
 import torch.optim as optim
-from torchvision import transforms
+from torchvision import models, transforms
 from PIL import Image, ImageDraw, ImageFilter
 import noise
 import numpy
@@ -50,6 +50,7 @@ class ImageDataset(Dataset):
 # google gemini pro 2.5 advanced code
 # edited by me
 
+
 def hex_to_rgb(hex):
     """Takes a hex string and returns the corresponding RGB tuple.
 
@@ -61,7 +62,8 @@ def hex_to_rgb(hex):
     Returns:
         int tuple, formatted (AA, BB, CC) but converted to the corresponding integer value.
     """
-    return tuple(int(hex[i:i+2], 16) for i in (0, 2, 4))
+    return tuple(int(hex[i : i + 2], 16) for i in (0, 2, 4))
+
 
 def generate_perlin_noise_image(
     size,
@@ -190,17 +192,27 @@ class CombineWithClouds:
         sampling_image = crop_to_center_circle(main_image)
         # Pick lower bound
         while True:
-            random_coordinates = (random.randint(0, main_image.size[0] - 1), random.randint(0, main_image.size[0] - 1))
+            random_coordinates = (
+                random.randint(0, main_image.size[0] - 1),
+                random.randint(0, main_image.size[0] - 1),
+            )
             lower_bound = list(sampling_image.getpixel(random_coordinates))
             if not lower_bound[3] == 0:
                 cloud_dimness = random.randint(20, 100)
                 for i in range(len(lower_bound)):
-                    lower_bound[i] = lower_bound[i] - cloud_dimness if lower_bound[i] > cloud_dimness else 0
+                    lower_bound[i] = (
+                        lower_bound[i] - cloud_dimness
+                        if lower_bound[i] > cloud_dimness
+                        else 0
+                    )
                 # print(lower_bound)
                 break
         # Pick upper bound
         while True:
-            random_coordinates = (random.randint(0, main_image.size[0] - 1), random.randint(0, main_image.size[0] - 1))
+            random_coordinates = (
+                random.randint(0, main_image.size[0] - 1),
+                random.randint(0, main_image.size[0] - 1),
+            )
             upper_bound = list(sampling_image.getpixel(random_coordinates))
             if not upper_bound[3] == 0:
                 upper_bound[3] = 0
@@ -208,7 +220,11 @@ class CombineWithClouds:
                 cloud_brightness = random.randint(50, 150)
                 for i in range(len(upper_bound)):
                     # print(upper_bound[i])
-                    upper_bound[i] = upper_bound[i] + cloud_brightness if upper_bound[i] < (255 - cloud_brightness) else 255
+                    upper_bound[i] = (
+                        upper_bound[i] + cloud_brightness
+                        if upper_bound[i] < (255 - cloud_brightness)
+                        else 255
+                    )
                     # print(upper_bound[i])
                 break
 
@@ -240,7 +256,9 @@ class CombineWithClouds:
         cloud_layer = Image.merge("RGBA", (r, g, b, final_alpha))
 
         combined_image = Image.alpha_composite(main_image, cloud_layer)
-        blurred_image = combined_image.filter(ImageFilter.GaussianBlur(radius = random.randint(0, 4)))
+        blurred_image = combined_image.filter(
+            ImageFilter.GaussianBlur(radius=random.randint(0, 4))
+        )
         blurred_center = crop_to_center_circle(blurred_image)
 
         combined_image = Image.alpha_composite(main_image, blurred_center)
@@ -276,6 +294,153 @@ class RandomApplyTransforms:
         sample = torch.clamp(sample, 0.0, 1.0)
 
         return sample
+
+
+# VGG loss, implemented on https://github.com/crowsonkb/vgg_loss?tab=readme-ov-file
+
+
+class Lambda(nn.Module):
+    """Wraps a callable in an :class:`nn.Module` without registering it."""
+
+    def __init__(self, func):
+        super().__init__()
+        object.__setattr__(self, "forward", func)
+
+    def extra_repr(self):
+        return getattr(self.forward, "__name__", type(self.forward).__name__) + "()"
+
+
+class WeightedLoss(nn.ModuleList):
+    """A weighted combination of multiple loss functions."""
+
+    def __init__(self, losses, weights, verbose=False):
+        super().__init__()
+        for loss in losses:
+            self.append(loss if isinstance(loss, nn.Module) else Lambda(loss))
+        self.weights = weights
+        self.verbose = verbose
+
+    def _print_losses(self, losses):
+        for i, loss in enumerate(losses):
+            print(f"({i}) {type(self[i]).__name__}: {loss.item()}")
+
+    def forward(self, *args, **kwargs):
+        losses = []
+        for loss, weight in zip(self, self.weights):
+            losses.append(loss(*args, **kwargs) * weight)
+        if self.verbose:
+            self._print_losses(losses)
+        return sum(losses)
+
+
+class TVLoss(nn.Module):
+    """Total variation loss (Lp penalty on image gradient magnitude).
+
+    The input must be 4D. If a target (second parameter) is passed in, it is
+    ignored.
+
+    ``p=1`` yields the vectorial total variation norm. It is a generalization
+    of the originally proposed (isotropic) 2D total variation norm (see
+    (see https://en.wikipedia.org/wiki/Total_variation_denoising) for color
+    images. On images with a single channel it is equal to the 2D TV norm.
+
+    ``p=2`` yields a variant that is often used for smoothing out noise in
+    reconstructions of images from neural network feature maps (see Mahendran
+    and Vevaldi, "Understanding Deep Image Representations by Inverting
+    Them", https://arxiv.org/abs/1412.0035)
+
+    :attr:`reduction` can be set to ``'mean'``, ``'sum'``, or ``'none'``
+    similarly to the loss functions in :mod:`torch.nn`. The default is
+    ``'mean'``.
+    """
+
+    def __init__(self, p, reduction="mean", eps=1e-8):
+        super().__init__()
+        if p not in {1, 2}:
+            raise ValueError("p must be 1 or 2")
+        if reduction not in {"mean", "sum", "none"}:
+            raise ValueError("reduction must be 'mean', 'sum', or 'none'")
+        self.p = p
+        self.reduction = reduction
+        self.eps = eps
+
+    def forward(self, input, target=None):
+        input = F.pad(input, (0, 1, 0, 1), "replicate")
+        x_diff = input[..., :-1, :-1] - input[..., :-1, 1:]
+        y_diff = input[..., :-1, :-1] - input[..., 1:, :-1]
+        diff = x_diff**2 + y_diff**2
+        if self.p == 1:
+            diff = (diff + self.eps).mean(dim=1, keepdims=True).sqrt()
+        if self.reduction == "mean":
+            return diff.mean()
+        if self.reduction == "sum":
+            return diff.sum()
+        return diff
+
+
+class VGGLoss(nn.Module):
+    """Computes the VGG perceptual loss between two batches of images.
+
+    The input and target must be 4D tensors with three channels
+    ``(B, 3, H, W)`` and must have equivalent shapes. Pixel values should be
+    normalized to the range 0–1.
+
+    The VGG perceptual loss is the mean squared difference between the features
+    computed for the input and target at layer :attr:`layer` (default 8, or
+    ``relu2_2``) of the pretrained model specified by :attr:`model` (either
+    ``'vgg16'`` (default) or ``'vgg19'``).
+
+    If :attr:`shift` is nonzero, a random shift of at most :attr:`shift`
+    pixels in both height and width will be applied to all images in the input
+    and target. The shift will only be applied when the loss function is in
+    training mode, and will not be applied if a precomputed feature map is
+    supplied as the target.
+
+    :attr:`reduction` can be set to ``'mean'``, ``'sum'``, or ``'none'``
+    similarly to the loss functions in :mod:`torch.nn`. The default is
+    ``'mean'``.
+
+    :meth:`get_features()` may be used to precompute the features for the
+    target, to speed up the case where inputs are compared against the same
+    target over and over. To use the precomputed features, pass them in as
+    :attr:`target` and set :attr:`target_is_features` to :code:`True`.
+
+    Instances of :class:`VGGLoss` must be manually converted to the same
+    device and dtype as their inputs.
+    """
+
+    models = {"vgg16": models.vgg16, "vgg19": models.vgg19}
+
+    def __init__(self, model="vgg16", layer=8, shift=0, reduction="mean"):
+        super().__init__()
+        self.shift = shift
+        self.reduction = reduction
+        self.normalize = transforms.Normalize(
+            mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+        )
+        self.model = self.models[model](pretrained=True).features[: layer + 1]
+        self.model.eval()
+        self.model.requires_grad_(False)
+
+    def get_features(self, input):
+        return self.model(self.normalize(input))
+
+    def train(self, mode=True):
+        self.training = mode
+
+    def forward(self, input, target, target_is_features=False):
+        if target_is_features:
+            input_feats = self.get_features(input)
+            target_feats = target
+        else:
+            sep = input.shape[0]
+            batch = torch.cat([input, target])
+            if self.shift and self.training:
+                padded = F.pad(batch, [self.shift] * 4, mode="replicate")
+                batch = transforms.RandomCrop(batch.shape[2:])(padded)
+            feats = self.get_features(batch)
+            input_feats, target_feats = feats[:sep], feats[sep:]
+        return F.mse_loss(input_feats, target_feats, reduction=self.reduction)
 
 
 # Model definition
@@ -629,6 +794,8 @@ def train_model(
         model = model.to(device)
 
     criterion = nn.L1Loss()
+    vgg_loss = VGGLoss().to(settings.DEVICE_IDS if settings.USE_DEVICE_IDS else device)
+
     optimizer = optim.Adam(model.parameters(), lr=settings.LEARNING_RATE)
     scheduler = optim.lr_scheduler.ReduceLROnPlateau(
         optimizer,
@@ -674,8 +841,11 @@ def train_model(
                 loss.backward()
                 torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
                 optimizer.step()
+            current_loss = vgg_loss(
+                inputs, vgg_loss.get_features(targets), target_is_features=True
+            )
 
-            running_loss += loss.item() * inputs.size(0)
+            running_loss += current_loss * inputs.size(0)
 
             if (i + 1) % 20 == 0 or (i + 1) == len(train_dataloader):
                 batch_time = time.time() - batch_start_time
@@ -721,7 +891,12 @@ def train_model(
                 else:
                     outputs = model(inputs)
                     loss = criterion(outputs, targets)
-                running_val_loss += loss.item() * inputs.size(0)
+                # running_val_loss += loss.item() * inputs.size(0)
+                current_loss = vgg_loss(
+                    inputs, vgg_loss.get_features(targets), target_is_features=True
+                )
+
+                running_loss += current_loss * inputs.size(0)
 
         epoch_val_loss = running_val_loss / len(valid_dataset)
         print(f"Epoch {epoch+1} [Val]   Avg Loss: {epoch_val_loss:.4f}")
@@ -790,6 +965,6 @@ def show_tensor_image(tensor):
 if __name__ == "__main__":
     train_model(
         DATA_DIR=os.path.join("data", "images"),
-        previous_model_path=os.path.join("models", "64_checkpoint_best.pth"),
+        # previous_model_path=os.path.join("models", "64_checkpoint_best.pth"),
         debug=False,
     )
