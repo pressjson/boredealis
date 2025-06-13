75d35940bb7af108febd9d83fe7a779130f0606f
Jason Press <jasonpress@duck.com>
2025-06-12 20:12:59 -0700

    Yet more tweaks. I really should use a beta branch or smth

--- network.py
+++ network.py
@@ -300,83 +300,83 @@ class RandomApplyTransforms:
 # @TODO: refactor this to a new file
 
 
-class Lambda(nn.Module):
-    """Wraps a callable in an :class:`nn.Module` without registering it."""
+# class Lambda(nn.Module):
+#     """Wraps a callable in an :class:`nn.Module` without registering it."""
 
-    def __init__(self, func):
-        super().__init__()
-        object.__setattr__(self, "forward", func)
+#     def __init__(self, func):
+#         super().__init__()
+#         object.__setattr__(self, "forward", func)
 
-    def extra_repr(self):
-        return getattr(self.forward, "__name__", type(self.forward).__name__) + "()"
-
-
-class WeightedLoss(nn.ModuleList):
-    """A weighted combination of multiple loss functions."""
-
-    def __init__(self, losses, weights, verbose=False):
-        super().__init__()
-        for loss in losses:
-            self.append(loss if isinstance(loss, nn.Module) else Lambda(loss))
-        self.weights = weights
-        self.verbose = verbose
-
-    def _print_losses(self, losses):
-        for i, loss in enumerate(losses):
-            print(f"({i}) {type(self[i]).__name__}: {loss.item()}")
-
-    def forward(self, *args, **kwargs):
-        losses = []
-        for loss, weight in zip(self, self.weights):
-            losses.append(loss(*args, **kwargs) * weight)
-        if self.verbose:
-            self._print_losses(losses)
-        return sum(losses)
+#     def extra_repr(self):
+#         return getattr(self.forward, "__name__", type(self.forward).__name__) + "()"
 
 
-class TVLoss(nn.Module):
-    """Total variation loss (Lp penalty on image gradient magnitude).
+# class WeightedLoss(nn.ModuleList):
+#     """A weighted combination of multiple loss functions."""
 
-    The input must be 4D. If a target (second parameter) is passed in, it is
-    ignored.
+#     def __init__(self, losses, weights, verbose=False):
+#         super().__init__()
+#         for loss in losses:
+#             self.append(loss if isinstance(loss, nn.Module) else Lambda(loss))
+#         self.weights = weights
+#         self.verbose = verbose
 
-    ``p=1`` yields the vectorial total variation norm. It is a generalization
-    of the originally proposed (isotropic) 2D total variation norm (see
-    (see https://en.wikipedia.org/wiki/Total_variation_denoising) for color
-    images. On images with a single channel it is equal to the 2D TV norm.
+#     def _print_losses(self, losses):
+#         for i, loss in enumerate(losses):
+#             print(f"({i}) {type(self[i]).__name__}: {loss.item()}")
 
-    ``p=2`` yields a variant that is often used for smoothing out noise in
-    reconstructions of images from neural network feature maps (see Mahendran
-    and Vevaldi, "Understanding Deep Image Representations by Inverting
-    Them", https://arxiv.org/abs/1412.0035)
+#     def forward(self, *args, **kwargs):
+#         losses = []
+#         for loss, weight in zip(self, self.weights):
+#             losses.append(loss(*args, **kwargs) * weight)
+#         if self.verbose:
+#             self._print_losses(losses)
+#         return sum(losses)
 
-    :attr:`reduction` can be set to ``'mean'``, ``'sum'``, or ``'none'``
-    similarly to the loss functions in :mod:`torch.nn`. The default is
-    ``'mean'``.
-    """
 
-    def __init__(self, p, reduction="mean", eps=1e-8):
-        super().__init__()
-        if p not in {1, 2}:
-            raise ValueError("p must be 1 or 2")
-        if reduction not in {"mean", "sum", "none"}:
-            raise ValueError("reduction must be 'mean', 'sum', or 'none'")
-        self.p = p
-        self.reduction = reduction
-        self.eps = eps
+# class TVLoss(nn.Module):
+#     """Total variation loss (Lp penalty on image gradient magnitude).
 
-    def forward(self, input, target=None):
-        input = F.pad(input, (0, 1, 0, 1), "replicate")
-        x_diff = input[..., :-1, :-1] - input[..., :-1, 1:]
-        y_diff = input[..., :-1, :-1] - input[..., 1:, :-1]
-        diff = x_diff**2 + y_diff**2
-        if self.p == 1:
-            diff = (diff + self.eps).mean(dim=1, keepdims=True).sqrt()
-        if self.reduction == "mean":
-            return diff.mean()
-        if self.reduction == "sum":
-            return diff.sum()
-        return diff
+#     The input must be 4D. If a target (second parameter) is passed in, it is
+#     ignored.
+
+#     ``p=1`` yields the vectorial total variation norm. It is a generalization
+#     of the originally proposed (isotropic) 2D total variation norm (see
+#     (see https://en.wikipedia.org/wiki/Total_variation_denoising) for color
+#     images. On images with a single channel it is equal to the 2D TV norm.
+
+#     ``p=2`` yields a variant that is often used for smoothing out noise in
+#     reconstructions of images from neural network feature maps (see Mahendran
+#     and Vevaldi, "Understanding Deep Image Representations by Inverting
+#     Them", https://arxiv.org/abs/1412.0035)
+
+#     :attr:`reduction` can be set to ``'mean'``, ``'sum'``, or ``'none'``
+#     similarly to the loss functions in :mod:`torch.nn`. The default is
+#     ``'mean'``.
+#     """
+
+#     def __init__(self, p, reduction="mean", eps=1e-8):
+#         super().__init__()
+#         if p not in {1, 2}:
+#             raise ValueError("p must be 1 or 2")
+#         if reduction not in {"mean", "sum", "none"}:
+#             raise ValueError("reduction must be 'mean', 'sum', or 'none'")
+#         self.p = p
+#         self.reduction = reduction
+#         self.eps = eps
+
+#     def forward(self, input, target=None):
+#         input = F.pad(input, (0, 1, 0, 1), "replicate")
+#         x_diff = input[..., :-1, :-1] - input[..., :-1, 1:]
+#         y_diff = input[..., :-1, :-1] - input[..., 1:, :-1]
+#         diff = x_diff**2 + y_diff**2
+#         if self.p == 1:
+#             diff = (diff + self.eps).mean(dim=1, keepdims=True).sqrt()
+#         if self.reduction == "mean":
+#             return diff.mean()
+#         if self.reduction == "sum":
+#             return diff.sum()
+#         return diff
 
 
 class VGGLoss(nn.Module):
@@ -850,7 +850,7 @@ def train_model(
                             vgg_loss_crit.get_features(targets),
                             target_is_features=True,
                         )
-                loss = l1_loss * L1_WEIGHT + vgg_loss * VGG_WEIGHT
+                loss = l1_loss * L1_WEIGHT + vgg_loss.to*(l1_loss.device) * VGG_WEIGHT
                 scaler.scale(loss).backward()
                 torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
                 scaler.step(optimizer)
@@ -872,7 +872,7 @@ def train_model(
                         vgg_loss_crit.get_features(targets),
                         target_is_features=True,
                     )
-                loss = l1_loss * L1_WEIGHT + vgg_loss * VGG_WEIGHT
+                loss = l1_loss * L1_WEIGHT + vgg_loss.to*(l1_loss.device) * VGG_WEIGHT
                 loss.backward()
                 torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
                 optimizer.step()
